ea3e9976404ef04d8ebaee2e1ddb9a3097ea5796
Namita Davey <ndavey24@gmail.com>
2019-06-25 17:02:16 -0700

    Initial commit

---
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.todoapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvitems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // obtain a reference to the ListView created with the layout
+        lvitems = (ListView) findViewById( R.id.lvitems);
+
+        // initialize the items list
+        readItems();
+
+        // initialize the adapter using the items list
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+
+        // wire the adapter to the view
+        lvitems.setAdapter(itemsAdapter);
+
+        //mock data
+        //items.add("First item");
+        //items.add("Second item");
+
+        // setup the listener on creation
+        setupListViewListener();
+    }
+
+
+    private void setupListViewListener() {
+        // set the ListView's itemLongClickListener
+        lvitems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                // remove the item in the list at the index given by position
+                items.remove(position);
+                // notify the adapter that the underlying dataset changed
+                itemsAdapter.notifyDataSetChanged();
+                // store the updated list
+                writeItems();
+                Log.i("MainActivity", "Removed item " + position);
+                // return true to tell the framework that the long click was consumed
+                return true;
+            }
+        });
+    }
+
+    public void onAddItem(View v) {
+        // obtain a reference to the EditText created with the layout
+        EditText etItemName = (EditText) findViewById(R.id.etItemName);
+        // grab the EditText's content as a String
+        String itemText = etItemName.getText().toString();
+        // add the item to the list via the adapter
+        itemsAdapter.add(itemText);
+        // store the updated list
+        writeItems();
+        // clear the EditText by setting it to an empty String
+        etItemName.setText("");
+        // display a notification to the user
+        Toast.makeText(getApplicationContext(), "Item added to list", Toast.LENGTH_SHORT).show();
+    }
+
+
+    // returns the file in which the data is stored
+    private File getDataFile() {
+        return new File(getFilesDir(), "todo.txt");
+    }
+
+    // read the items from the file system
+    private void readItems() {
+        try {
+            // create the array using the content in the file
+            items = new ArrayList<String>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            // print the error to the console
+            e.printStackTrace();
+            // just load an empty list
+            items = new ArrayList<>();
+        }
+    }
+
+    // write the items to the filesystem
+    private void writeItems() {
+        try {
+            // save the item list as a line-delimited text file
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            // print the error to the console
+            e.printStackTrace();
+        }
+    }
+}
